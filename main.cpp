30e0e278d64db946d4f66b47e3244884f9d5089d
danafisk <dfisk@csuchico.edu>
2018-09-26 16:21:28 -0700

    asks if you want to add items to my grocery list

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//Autor: Dana Fisk
+
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +9,15 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+do
+{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+}while((input !='Q') && ( input !='q'));
 
   return 0;
 }
