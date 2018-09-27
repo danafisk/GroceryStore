21e29f4cb0c34604e44ff3cd2aacb3d8e4975e56
danafisk <dfisk@csuchico.edu>
2018-09-26 17:51:08 -0700

    grocery list

--- main.cpp
+++ main.cpp
@@ -9,15 +9,50 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-do
-{
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-}while((input !='Q') && ( input !='q'));
+  do
+  {
+    cout<<"\n==GROCERY LIST MANAGER==";
+    cout<<"\nEnter your choice: ";
+    cout<<"\n (A)dd an item";
+    cout<<"\n (Q)uit";
+    cout<<"\nYour choice (A/Q): ";
+    cin>>input;
+    if ((input == 'A') || (input == 'a'))  {
+      string newitem;
+      cout<<"\nWhat is the item?\n";
+      cin>> newitem;
+      if (numItems < 5) {
+      list[numItems] = newitem;
+      numItems++;
+      cout<<numItems;
+      }
 
-  return 0;
-}
+      //doesnt work   
+      //cout << "You have " << list.length() - numItems << " spaces left in your list";
+    
+
+   else 
+        { cout<< "You'll need a bigger list!\n";
+        }
+    }
+
+
+
+        }while((input !='Q') && ( input !='q'));
+
+        return 0;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
